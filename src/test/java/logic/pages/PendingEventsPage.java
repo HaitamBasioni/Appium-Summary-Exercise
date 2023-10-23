d7c6dc817b776a9274a545f8d3a9a52929f2f901
Haitam Basioni <hitam.b@outlook.com>
2023-10-23 03:54:40 +0300

    edit PedingEventPage

--- src/test/java/logic/pages/PendingEventsPage.java
+++ src/test/java/logic/pages/PendingEventsPage.java
@@ -2,20 +2,21 @@ package logic.pages;
 
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
 
 
 public class PendingEventsPage extends BasePage{
-    private MobileElement event;
-
+    private final By EVENT_TITTLE = By.id("com.claudivan.taskagenda:id/tvTitulo");
+    private MobileElement eventTitle;
     public PendingEventsPage(AndroidDriver<MobileElement> driver) {
         super(driver);
     }
     public String getEventName(String eventName){
-        event = scrollDownToElement(eventName);
-        return event.getText();
+        eventTitle = scrollDownToElement(eventName);
+        return eventTitle.getText();
     }
     public void clickEventByName(String eventName){
-        event= scrollDownToElement(eventName);
-        event.click();
+        eventTitle = scrollDownToElement(eventName);
+        eventTitle.click();
     }
 }
